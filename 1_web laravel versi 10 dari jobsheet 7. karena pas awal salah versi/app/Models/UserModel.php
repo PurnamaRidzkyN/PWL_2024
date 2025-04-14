cd8f0453b7f2d89d0b3eef2fd62fc886510c866b
Purnama Ridzky Nugraha <purnamanugraha492@gmail.com>
2025-04-14 16:04:10 +0700

    penambahan laravel versi 10

---
+++ 1_web laravel versi 10 dari jobsheet 7. karena pas awal salah versi/app/Models/UserModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class UserModel extends Authenticatable
+{
+    use HasFactory;
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $hidden = ['password'];
+    protected $casts = ['password' => 'hashed'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level(): BelongsTo
+    {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+
+    public function stok()
+    {
+        return $this->hasMany(StokModel::class, 'user_id', 'user_id');
+    }
+
+    public function getRoleName(): string{
+        return $this->level->level_nama;
+    }
+    public function hasRole($role):bool{
+        return  $this->level->level_kode == $role;
+    }
+}
